ef4575a24506bc5c996b656c7767c0a9029a3f8e
st0neshi <imericshi@gmail.com>
2021-07-28 13:34:07 -0400

    finished the fomatting

---
+++ Gallery/gallery.js
@@ -0,0 +1,15 @@
+document.querySelector("#prev").addEventListener('click', function() {
+    document.getElementById("img").src = "./imgs/burger-2.jpg";
+})
+
+const myGallery = ["./imgs/burger-1.jpg", "./imgs/burger-2.jpg", "./imgs/chicken-wings-1.jpg"];
+
+function Next(array) {
+    for (i = 0; i < array.length; i++) {
+        
+    }
+}
+
+function Prev(array) {
+    
+} 
